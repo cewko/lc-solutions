20d3b1c8892d3f2b4ff75d0ae15c187efaa4ae4d
cewko <221901489+cewko@users.noreply.github.com>
2026-01-15 16:38:35 +0100

    146. LRU Cache

---
+++ medium/146/main.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.lru, self.mru = Node(0, 0), Node(0, 0)
+        self.lru.next, self.mru.prev = self.mru, self.lru
+
+    def remove(self, node):
+        prev, next_ = node.prev, node.next
+        prev.next, next_.prev = next_, prev
+
+    def insert(self, node):
+        prev, next_ = self.mru.prev, self.mru
+        prev.next = next_.prev = node
+        node.next, node.prev = next_, prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            lru = self.lru.next
+            self.remove(lru)
+            del self.cache[lru.key]
